5c0c27ab6ec9d025b4f3391f6fe6cd34958b40c4
Benjamin Uriarte <Benjamin Uriarte>
2017-04-24 21:20:44 -0700

    These changes will remove tools from menubar

--- JChessRevised/src/MainFrame/ChessMenuBar/Chess_MainMenuBar.java
+++ JChessRevised/src/MainFrame/ChessMenuBar/Chess_MainMenuBar.java
@@ -5,13 +5,13 @@ import javax.swing.JMenuBar;
 import MainFrame.ChessFrame.MainFrame;
 import MainFrame.ChessMenuBar.ChessBar_Menus.Edit_Menu;
 import MainFrame.ChessMenuBar.ChessBar_Menus.File_Menu;
-import MainFrame.ChessMenuBar.ChessBar_Menus.Tools_Menu;
+
 
 public class Chess_MainMenuBar extends JMenuBar {
 
     private final File_Menu Fmenu;
     private final Edit_Menu Emenu = new Edit_Menu();
-    private final Tools_Menu Tmenu = new Tools_Menu();
+
 
 
     /**
@@ -21,7 +21,6 @@ public class Chess_MainMenuBar extends JMenuBar {
         Fmenu = new File_Menu(ff);
         add(Fmenu);
         add(Emenu);
-        add(Tmenu);
 
 
     }
