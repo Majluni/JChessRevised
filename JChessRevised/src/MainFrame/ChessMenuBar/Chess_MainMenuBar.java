b84d742aee25083b4190ae98dd7b211861f593f0
Benjamin Uriarte <Benjamin Uriarte>
2017-04-24 21:13:27 -0700

    These changes will remove help from the menubar

--- JChessRevised/src/MainFrame/ChessMenuBar/Chess_MainMenuBar.java
+++ JChessRevised/src/MainFrame/ChessMenuBar/Chess_MainMenuBar.java
@@ -5,7 +5,6 @@ import javax.swing.JMenuBar;
 import MainFrame.ChessFrame.MainFrame;
 import MainFrame.ChessMenuBar.ChessBar_Menus.Edit_Menu;
 import MainFrame.ChessMenuBar.ChessBar_Menus.File_Menu;
-import MainFrame.ChessMenuBar.ChessBar_Menus.Help_Menu;
 import MainFrame.ChessMenuBar.ChessBar_Menus.Tools_Menu;
 
 public class Chess_MainMenuBar extends JMenuBar {
@@ -14,7 +13,6 @@ public class Chess_MainMenuBar extends JMenuBar {
     private final Edit_Menu Emenu = new Edit_Menu();
     private final Tools_Menu Tmenu = new Tools_Menu();
 
-    private final Help_Menu Hmenu = new Help_Menu();
 
     /**
      * Creates a new instance of Chess_MainMenuBar
@@ -24,7 +22,7 @@ public class Chess_MainMenuBar extends JMenuBar {
         add(Fmenu);
         add(Emenu);
         add(Tmenu);
-        add(Hmenu);
+
 
     }
 
